e9aed103f004423941eb687a9828e6a475be5a02
john050481 <john0504@yandex.ru>
2020-04-26 18:41:51 +0300

    fix

--- frontend/src/Component/UserProfile/ReauthenticateWithCredential/index.js
+++ frontend/src/Component/UserProfile/ReauthenticateWithCredential/index.js
@@ -1,14 +1,19 @@
 import './style.css'
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import ModalApp from "../../Modal";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 function ReauthenticateWithCredential({openModal, setOpenModal, callback}) {
+    const inputEmail = useRef(null);
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    useEffect( () => {
+        if (openModal) inputEmail.current.focus();
+    }, [openModal])
 
     useEffect( () => {
         setEmail('');
@@ -28,7 +33,7 @@ function ReauthenticateWithCredential({openModal, setOpenModal, callback}) {
             <Modal.Body>
                 <Form.Group>
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter current email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                    <Form.Control ref={inputEmail} type="email" placeholder="Enter current email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Password</Form.Label>
