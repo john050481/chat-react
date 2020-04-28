3286707d99dce145c00ecc961d10aa580b590658
john050481 <john0504@yandex.ru>
2020-04-28 12:51:10 +0300

    fix

--- frontend/src/Component/Layout/Chat/MassageBlock/index.js
+++ frontend/src/Component/Layout/Chat/MassageBlock/index.js
@@ -1,8 +1,9 @@
 import './style.css'
 import React from 'react'
 import Citation from '../Citation'
+import {connect} from "react-redux";
 
-export default function MessageBlock(props) {
+function MessageBlock(props) {
 
     function handleClick(e) {
         let target = e.target;
@@ -16,6 +17,13 @@ export default function MessageBlock(props) {
         <main className="content message-block-wrapper" onClick={handleClick} >
             <div id='message-block' className='content message-block'>
                 {/*тут будут сообщения!!!*/}
+                {
+                    props.messages.map( message =>
+                        <div key={message.id} className='message-wrap left-message left-color' data-message={true}>
+                            {message.body}
+                        </div>
+                    )
+                }
             </div>
             {
                 props.citation
@@ -27,3 +35,13 @@ export default function MessageBlock(props) {
         </main>
     )
 }
+
+const mapStateToProps = store => {
+    return {
+        messages: store.chat.messages
+    }
+}
+const mapDispatchToProps = {
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageBlock)
