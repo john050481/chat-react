25ccc02702484bcfffbc3d0fccfb33da693ad0e1
john050481 <john0504@yandex.ru>
2020-05-02 01:02:21 +0300

    fix

--- frontend/src/Component/UserProfile/db.js
+++ frontend/src/Component/UserProfile/db.js
@@ -24,12 +24,14 @@ function handlerDb(auth, users) {
             });
         });
 
-    db.collection("chats").where("state", "==", "CA").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id);
-            console.dir(doc.data());
-        });
-    });
+    db.collection("chats").where("state", "==", "CA").get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id);
+                console.dir(doc.data());
+            });
+        })
+        .catch(e => console.log(e));
 
     // Add a new document in collection "chats"
     db.collection("chats").doc("LA").set({
@@ -47,7 +49,7 @@ function handlerDb(auth, users) {
 
     // Add a new document in collection "users"
     users.forEach( user => {
-        db.collection("users").doc(user.email).set({...user})
+        db.collection("users").doc(user.email).set({...user}, { merge: true })
             .then(function() {
                 console.log("Document successfully written!");
             })
@@ -61,26 +63,28 @@ function handlerDb2(auth, users) {
     const db = auth.firebase.firestore();
     console.log(auth);
 
-    db.collection("users").doc("Chaim_McDermott@dana.io").get().then((querySnapshot) => {
-        let user = querySnapshot.data();
+    db.collection("users").doc("Chaim_McDermott@dana.io").get()
+        .then((querySnapshot) => {
+            let user = querySnapshot.data();
 
-        user.chats[0].get().then( querySnapshot => {
-            let chat = querySnapshot.data();
-            console.log(querySnapshot.id, chat);
-        })
+            user.chats[0].get().then( querySnapshot => {
+                let chat = querySnapshot.data();
+                console.log(querySnapshot.id, chat);
+            })
 
-        // https://firebase.google.com/docs/firestore/data-model
-        // var messageRef = db.collection('rooms').doc('roomA').collection('messages').doc('message1');
-        user.chats[0].collection("messages").get().then( querySnapshot => {
-            querySnapshot.forEach(function(doc) {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
-                doc.data().from.get().then(querySnapshot => {
-                    console.log(querySnapshot.data())
-                })
-            });
+            // https://firebase.google.com/docs/firestore/data-model
+            // var messageRef = db.collection('rooms').doc('roomA').collection('messages').doc('message1');
+            user.chats[0].collection("messages").get().then( querySnapshot => {
+                querySnapshot.forEach(function(doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data());
+                    doc.data().from.get().then(querySnapshot => {
+                        console.log(querySnapshot.data())
+                    })
+                });
+            })
         })
-    });
+        .catch(e => console.log(e));
 }
 
 function Db(props) {
