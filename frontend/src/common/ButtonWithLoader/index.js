863fe138ded626f334e64a8343af8237ebe85392
john050481 <john0504@yandex.ru>
2020-05-02 12:09:54 +0300

    fix

--- frontend/src/common/ButtonWithLoader/index.js
+++ frontend/src/common/ButtonWithLoader/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
 import Spinner from "react-bootstrap/Spinner";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 export default function ButtonWithLoader({variant, onClick, visibleLoader, children}) {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Button variant={variant} onClick={onClick}>
-                        {children}
-                    </Button>
-                </Col>
+        <Button variant={variant} onClick={onClick}>
             {
                 visibleLoader &&
-                <Col>
-                    <Spinner variant={variant} animation="border" />
-                </Col>
+                <Spinner
+                    as="span"
+                    animation="grow"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                />
             }
-            </Row>
-        </Container>
+            {children}
+        </Button>
     )
 }
