2a9f01a8bc11864b34d98542875a3e401c6c6a3a
john050481 <john0504@ya.ru>
2020-05-07 13:20:00 +0300

    fix

--- frontend/src/model/roomMessages.js
+++ frontend/src/model/roomMessages.js
@@ -6,5 +6,7 @@ export default {
     userId: "", // The id of the user that sent the message.
     name: "", // The name of the user that sent the message.
     message: "", // The content of the message.
-    timestamp: "" // The time at which the message was sent. // firebase.firestore.FieldValue.serverTimestamp()
+    timestamp: "", // The time at which the message was sent. // firebase.firestore.FieldValue.serverTimestamp()
+    forwarded: false, // пересланное сообщение или нет
+    citationId: "" // id процитированного сообщения, если есть
 }
