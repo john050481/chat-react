3625eda052c1bb3f2797cae8fa40c5c22cbc073b
john050481 <john0504@ya.ru>
2020-05-07 09:47:13 +0300

    fix

--- frontend/src/Component/UserProfile/Profile/index.js
+++ frontend/src/Component/UserProfile/Profile/index.js
@@ -4,27 +4,29 @@ import {useChatFirebase} from "../../../hooks/useChatFirebase";
 
 export default function (props) {
     const chatDb = useChatFirebase();
+    const roomIdElem = document.getElementById('roomIdElem');
+    const userIdElem = document.getElementById('userIdElem');
 
     async function handlerGetInfo(e) {
-        console.log(chatDb, await chatDb.getUserData(chatDb.userId));
+        console.log(chatDb, await chatDb.getUserData(userIdElem.value ? userIdElem.value : chatDb.userId));
     }//*********
     function handlerCreate(e) {
-        chatDb.createUser('example@example.ru', 'example@example.ru', console.log)
-            .then( res => console.log('Создан пользователь = ', res))
+        chatDb.createUser(userIdElem.value ? userIdElem.value : 'example@example.ru', 'example@example.ru', console.log)
+            .then( res => console.log(`Создан пользователь = ${userIdElem.value ? userIdElem.value : 'example@example.ru'}`, res))
             .catch( e => console.log(e));
     }//*********
     function handlerUpdate(e) {
-        chatDb.updateUser( chatDb.userId,{ name: 'name', address: {city: 'blg'} }, console.log )
-            .then( () => console.log('Обновлен пользователь'))
+        chatDb.updateUser( userIdElem.value ? userIdElem.value : chatDb.userId,{ name: 'name', address: {city: 'blg'} }, console.log )
+            .then( () => console.log(`Обновлен пользователь ${userIdElem.value ? userIdElem.value : chatDb.userId}`))
             .catch( e => console.log(e));
     }//*********
     function handlerDelete(e) {
-        chatDb.deleteUser(chatDb.userId, console.log)
-            .then( () => console.log('Удален пользователь'))
+        chatDb.deleteUser(userIdElem.value ? userIdElem.value : chatDb.userId, console.log)
+            .then( () => console.log(`Удален пользователь ${userIdElem.value ? userIdElem.value : chatDb.userId}`))
             .catch( e => console.log(e));
     }//*********
     async function handlergetGetRoomMessages(e) {
-        let messages = await chatDb.getRoomMessages('room1');
+        let messages = await chatDb.getRoomMessages(roomIdElem.value);
         console.log(messages);
     }//*********
     async function handlercreateRoom(e) {
@@ -32,21 +34,21 @@ export default function (props) {
         console.log('result = ', await result);
     }//*********
     function handlergetRoomMetadata(e) {
-        chatDb.getRoomMetadata('room1', console.log);
+        chatDb.getRoomMetadata(roomIdElem.value, console.log);
     }//*********
     function handlerenterRoom(e) {
-        chatDb.enterRoom('room1').then( () => console.log('Enterd Room1!!') );
+        chatDb.enterRoom(roomIdElem.value).then( () => console.log(`Enterd ${roomIdElem.value}`) );
     }//*********
     function handlerleaveRoom(e) {
-        chatDb.leaveRoom('room1').then( () => console.log('Deleted Room1!!') );
+        chatDb.leaveRoom(roomIdElem.value).then( () => console.log(`Deleted ${roomIdElem.value}!!`) );
     }//*********
     async function handlerupdateRoomMetadata(e) {
-        chatDb.updateRoomMetadata('room1', 'new name room #1', 'public', console.log)
+        chatDb.updateRoomMetadata(roomIdElem.value, `new name room`, 'public', console.log)
         .then( () => console.log('Room обновлена!'))
         .catch( e => console.log(e))
     }//*********
     async function handlerDeleteRoom(e) {
-        chatDb.deleteRoom('JI5A3fqPTNkYNbfIobYG', console.log);
+        chatDb.deleteRoom(roomIdElem.value, console.log);
     }//!!!!!!!!! ПРОБЛЕМА, см. "useChatFirebase"
 
     return (
@@ -54,11 +56,15 @@ export default function (props) {
             Profile
             <DB />{/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
             <hr />
+            <label>user id: <input id={'userIdElem'} /></label>
+            <br />
             <button onClick={handlerGetInfo} className={'btn btn-outline-success'}>get info</button>
             <button onClick={handlerCreate} className={'btn btn-outline-success'}>create user</button>
             <button onClick={handlerUpdate} className={'btn btn-outline-success'}>updete user</button>
             <button onClick={handlerDelete} className={'btn btn-outline-success'}>delete user</button>
             <hr />
+            <label>room id: <input id={'roomIdElem'} /></label>
+            <br />
             <button onClick={handlergetGetRoomMessages} className={'btn btn-outline-success'}>get room messages</button>
             <br/>
             <button onClick={handlergetRoomMetadata} className={'btn btn-outline-success'}>get metadata room</button>
