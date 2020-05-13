ec9811c439b40e139a0d8114cdbc38d8118981c6
john050481 <john0504@yandex.ru>
2020-05-13 17:10:48 +0300

    fix

--- frontend/src/redux/sagas/requestUserRoomsMetadata/index.js
+++ frontend/src/redux/sagas/requestUserRoomsMetadata/index.js
@@ -1,19 +1,21 @@
 import {put, takeEvery, call} from 'redux-saga/effects'
-import {REQUEST_CHATS, FETCHED_CHATS} from '../../types';
+import {REQUEST_USERROOM, UPDATE_ROOMLIST} from '../../types';
 import {showLoader, hideLoader, showAlert} from '../../actions';
 
-//---REGUEST ALL CHATS---
-export default function* sagaWatcherRequestChats() {
-    yield takeEvery(REQUEST_CHATS, sagaWorkerRequestChats);
+//---REGUEST ALL ROOMS METADATA---
+export default function* sagaWatcherRequestUserRoomsMetadata(...args) {
+    console.log('########################### sagaWatcherRequestUserRoomsMetadata 111 === ', args);
+    yield takeEvery(REQUEST_USERROOM, sagaWorkerRequestUserRoomsMetadata, ...args);
 }
-function* sagaWorkerRequestChats() {
+function* sagaWorkerRequestUserRoomsMetadata(...args) {
+    console.log('########################### sagaWorkerRequestUserRoomsMetadata === 222 ', args);
     const url = `https://jsonplaceholder.typicode.com/users`;
 
     try {
         yield put(showLoader())
         let response = yield call(fetch, url);
         let chats = yield call(response.json.bind(response));
-        yield put({ type: FETCHED_CHATS, payload: chats });
+        yield put({ type: UPDATE_ROOMLIST, payload: chats });
         yield put(hideLoader());
         yield put(showAlert({text: 'FETCH CHATS DONE!!!', options: {variant: 'success'}}))
     } catch(e) {
