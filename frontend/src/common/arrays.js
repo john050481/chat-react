b02fffea3b461db66deb138dcfe00d4163a7ddeb
john050481 <john0504@yandex.ru>
2020-05-29 23:24:18 +0300

    fix

--- frontend/src/common/arrays.js
+++ frontend/src/common/arrays.js
@@ -21,7 +21,24 @@ arr2 = [1,3,6,9]
 console.log(diff(arr1, arr2)) // [ 2, 4, 5 ]
 console.log(diff(arr2, arr1)) // [ 6, 9 ]
 */
-
-export default function diff(target, external) {
+export function diffArrays(target, external) {
     return target.filter( i => external.indexOf(i) < 0 );
 };
+
+export function equalArrays(target, external) {
+    if (!Array.isArray(target) || !Array.isArray(external))
+        return false
+
+    if (target.length !== external.length)
+        return false
+
+    const diff1 = diffArrays(target, external);
+    if (diff1.length)
+        return false
+
+    const diff2 = diffArrays(external, target);
+    if (diff2.length)
+        return false
+
+    return true
+}
